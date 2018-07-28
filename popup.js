20ce41bc251757dad2d40d0792bb540649a09b20
Maytham <maythamsabbagh@gmail.com>
2018-07-28 12:42:53 -0400

    added uplifting stuff

--- popup.js
+++ popup.js
@@ -10,13 +10,26 @@ upliftMe.onclick = function(element) {
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 		chrome.tabs.executeScript(
 			tabs[0].id,
+			{code: 'document.body.style.backgroundImage = "url(\'https://image.ibb.co/niVxYo/habib_final_sticker_inverse.png\')";'},
+		);
+		chrome.tabs.executeScript(
+			tabs[0].id,
+			{code: 'document.body.style.backgroundSize = "500px 533px";'}
+		);
+    });
+
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+		chrome.tabs.executeScript(
+			tabs[0].id,
 			{file : '/jsonPost.js'}
 		);
 	});
 		
-		chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-			chrome.tabs.executeScript(
-				tabs[0].id,
-				{file: '/parsePage.js'});
-			});
-};
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+		chrome.tabs.executeScript(
+			tabs[0].id,
+			{code: '/parsePage.js'}
+		);
+	});
+
+;
